9025b2322946b0f811a28500bbb502435adedeba
Ivan Galinskiy <ivanko.rus@gmail.com>
2015-11-23 00:19:23 -0600

    Figured out the fft integration. TODO: double integration

--- fft_test.rs
+++ fft_test.rs
@@ -1,10 +1,10 @@
 # This file serves to understand the concept of FFT and the derivatives in it
 
-L = 1.0;
+L = 2.0;
 
 # The number of samples must be odd to avoid singularities from evaluating
 # 1/sinc(2*k/N)
-N = 1001;
+N = 100001;
 dx = L/N;
 
 p = float(pi)
@@ -31,26 +31,38 @@ end
 # an additional term of a_0*x which has to be Fourier-decomposed and added to
 # the final series
 
-# Single integral (integrates everything except the average term)
+# Single integral
 function intk!(arr)
+    # The DC term (it has to be accounted for in a different way, see below)
+    a0 = arr[1]
+    
     arr[1] = 0
     for k in 1:length(arr)-1
-        arr[k+1] = arr[k+1] / (im * k * (2*p/L) * sinc(2*k/N))
+        arr[k+1] = arr[k+1] / (im * k * (2*p/L) * sinc(2*k/N)) + im*a0*L/(p*k)
     end
 end
 
 # Double integral (integrates everything except the average term)
 function iintk!(arr)
+    # The DC term (it has to be accounted for in a different way, see below)
+    a0 = arr[1]
+    
     arr[1] = 0
     for k in 1:length(arr)-1
-        arr[k+1] = -arr[k+1] / (k * (2*p/L) * sinc(k/N))^2
+        arr[k+1] = -(arr[k+1]) / (k * (2*p/L) * sinc(k/N))^2
+        #arr[k+1] = arr[k+1] / (im * k * (2*p/L) * sinc(2*k/N)) + im*a0*L/(p*k)
+        #arr[k+1] = arr[k+1] / (im * k * (2*p/L) * sinc(2*k/N)) + im*a0*L/(p*k)
     end
 end
 
-z = convert(Array{Float64, 1}, [sin(10*p*(x/N)) for x in 1:N])
+z = convert(Array{Float64, 1}, [(x/N) for x in 1:N])
 zk = fft(z)
-
 iintk!(zk)
+ddifk!(zk)
+
+# Test of a linear function decomposition
+#zk = convert(Array{Complex128, 1}, zeros(N))
+#zk[2:end] = [im*N/(p*k) for k in 1:N-1]
 
 zd = real(ifft(zk))
-plot(zd[10:end-10])
+plot(zd[1000:end-1000])
